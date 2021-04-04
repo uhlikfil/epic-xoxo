3a2206b0b7d3f733571beef6b0e7c418961b1195
Filip <filipfilauhlik@gmail.com>
2021-04-04 17:53:21 +0200

    persistent database

--- microservices/user_svc/database/scheme.sql
+++ microservices/user_svc/database/scheme.sql
@@ -1,1 +1,6 @@
---- tady bude create table
+CREATE TABLE IF NOT EXISTS Users (
+    username varchar(64) NOT NULL,
+    email varchar(64) NOT NULL,
+    password_hash varchar(64) NOT NULL,
+    PRIMARY KEY (username)
+) ENGINE=InnoDB
