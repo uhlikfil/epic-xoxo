791e8fdd450267db8365d500ee3bd2187d0191d9
Filip <filipfilauhlik@gmail.com>
2021-04-06 17:38:35 +0200

    reverse black

--- microservices/user_svc/src/user_svc/database/queries.py
+++ microservices/user_svc/src/user_svc/database/queries.py
@@ -2,10 +2,8 @@ import os
 
 TABLE_NAME = os.getenv("TABLE_NAME")
 
-
+# fmt: off
 SELECT_USER = f"SELECT username, origin_ip, last_ip, UNIX_TIMESTAMP(created) FROM {TABLE_NAME} WHERE username = %s"
-INSERT_USER = (
-    f"INSERT INTO {TABLE_NAME} (username, origin_ip, last_ip) VALUES (%s, %s, %s)"
-)
+INSERT_USER = f"INSERT INTO {TABLE_NAME} (username, origin_ip, last_ip) VALUES (%s, %s, %s)"
 DELETE_USER = f"DELETE FROM {TABLE_NAME} WHERE username = %s"
 UPDATE_USER = f"UPDATE {TABLE_NAME} SET last_ip = %s WHERE username = %s"
