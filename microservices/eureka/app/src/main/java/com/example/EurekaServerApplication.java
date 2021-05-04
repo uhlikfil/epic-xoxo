2af44ba27f5fad64b7937fda14a628e2f6b2af83
Filip <filipfilauhlik@gmail.com>
2021-05-04 17:28:58 +0200

    eureka multistage build

--- microservices/eureka/app/src/main/java/com/example/EurekaServerApplication.java
+++ microservices/eureka/app/src/main/java/com/example/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package cz.neuvimat.eureka;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
-
 }
