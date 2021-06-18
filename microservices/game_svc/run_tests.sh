e1d6eb895be85e91ad485092575aa215b4dd9ad5
Matyáš Neuvirt <neuvimat@fel.cvut.cz>
2021-06-18 10:23:58 +0200

    final touches

--- microservices/game_svc/run_tests.sh
+++ microservices/game_svc/run_tests.sh
@@ -1,7 +1,8 @@
 BASEDIR=$(dirname "$0")
 
+cd $BASEDIR/game_svc_tests
 docker-compose build
-docker-compose -f docker-compose.tests.yml up --abort-on-container-exit --exit-code-from swa_game_service_test
+docker-compose -f up --abort-on-container-exit --exit-code-from swa_game_service_test
 docker-compose down -v
 
 exit $?
